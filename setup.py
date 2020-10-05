b0e080fcd1ba375f7bfc33ebd982474ca19c9db9
Namgyu Ho <itsnamgyu@gmail.com>
2020-10-05 20:46:24 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='deep-pyramidal-representations-person-re-identification',
+    version='',
+    packages=['src', 'src.ml', 'src.ml.net', 'src.ml.net.pt', 'src.ml.metric', 'src.reid', 'src.reid.reranking',
+              'src.utils', 'src.configs', 'src.results', 'src.features', 'src.datamanager', 'src.visualization'],
+    url='',
+    license='',
+    author='itsnamgyu',
+    author_email='',
+    description=''
+)
